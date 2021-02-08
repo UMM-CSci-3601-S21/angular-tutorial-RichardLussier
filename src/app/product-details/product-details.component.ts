9178798da238b00b9857c8f4b292bcd9047e56ac
Richard Lussier <lussi036@morris.umn.edu>
2021-02-08 00:09:53 -0600

    Add product info to product page
    Added product-specific information onto each page linked to a product. Each product page contains its name, price, and description (if applicable).

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //Getting the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //Find the product that corresponds ith the id provided in the route
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
